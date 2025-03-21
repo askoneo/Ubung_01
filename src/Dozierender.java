d738f4037cda02ca12e4445a3e8daac0218b2623
askoneo <marcelkaun41@gmail.com>
2025-03-21 11:31:28 +0100

    sus

---
+++ src/Dozierender.java
@@ -0,0 +1,15 @@
+public class Dozierender {
+    String name;
+    String fakultaet;
+    String bueronummer ;
+
+    public static void Doz(String name, String fakultaet, String bueronummer){
+     name = new String();
+     fakultaet = new String();;
+     bueronummer = new String();
+    }
+    public void frageBeantworten(){
+      // TODO document why this method is empty
+    }
+}
+
---
+++ src/Studierender.java
@@ -0,0 +1,14 @@
+public class Studierender {
+    String name;
+    String matrikelnummer;
+    String studiengang;
+    public static void Stud(String name, String matrikelnummer, String studiengang){
+    name = new String();
+    matrikelnummer = new String();
+    studiengang = new String();
+
+    }
+    public void fragestellen(){
+    
+    }
+}
