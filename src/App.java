979d825885c3bbe878db43cd6020f904c332be98
bjoernSWE <beh6943@thi.de>
2025-03-21 11:23:54 +0100

    ja

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
-    Dozierender dozierender;
+    
     
     public static void main(String[] args) throws Exception {
-        Dozierender dozierender = new Dozierender();
+    
     }
 }
