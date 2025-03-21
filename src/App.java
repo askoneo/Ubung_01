d7d28eaf80a3d28b3a2ca99c843dae5cd669414a
bjoernSWE <beh6943@thi.de>
2025-03-21 10:50:37 +0100

    Stud

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Amogus");
+        System.out.println("Hello, Studierender");
     }
 }
